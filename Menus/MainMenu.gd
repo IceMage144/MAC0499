00c236b7889ffd8fb1b0ad66275fcbaf587a258c
IceMage144 <jgabriel.basi@hotmail.com>
2019-07-15 02:02:34 -0300

    Add main menu entries and add my font

--- Menus/MainMenu.gd
+++ Menus/MainMenu.gd
@@ -1,3 +1,29 @@
 extends Control
 
-var debug_mode
+var debug_mode = false
+var focused_button_id = 0
+
+onready var buttons = $MarginContainer/VBoxContainer.get_children()
+
+func _ready():
+	for i in range(len(self.buttons)):
+		self.buttons[i].connect("mouse_entered", self, "_on_button_mouse_entered", [i])
+
+func _process(delta):
+	var focused_button = self.buttons[self.focused_button_id]
+	if not focused_button.is_hovered():
+		focused_button.release_focus()
+		focused_button.grab_focus()
+	if Input.is_action_just_pressed("ui_down") and self.focused_button_id < len(self.buttons) - 1:
+		self.focused_button_id += 1
+		while self.buttons[self.focused_button_id].disabled and self.focused_button_id < len(self.buttons) - 1:
+			self.focused_button_id += 1
+	if Input.is_action_just_pressed("ui_up") and self.focused_button_id > 0:
+		self.focused_button_id -= 1
+		while self.buttons[self.focused_button_id].disabled and self.focused_button_id > 0:
+			self.focused_button_id -= 1
+
+func _on_button_mouse_entered(button_id):
+	if not self.buttons[button_id].disabled:
+		self.buttons[self.focused_button_id].release_focus()
+		self.focused_button_id = button_id
